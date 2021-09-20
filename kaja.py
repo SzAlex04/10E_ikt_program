0ee6b822b6abfe64bdd01274fb504314d998e57a
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2021-09-20 10:29:46 +0200

    Add files via upload

---
+++ kaja.py
@@ -0,0 +1,10 @@
+def mirelit pizza (cheese_type; side, side_dish):
+    print('Kivesszük a fagyóból a pizzát és kibontjuk a csomagloását.')
+    print('Előmelegítjük a sütőt.')
+    print('Igény szerint lehet rá rakni feltéteket.')
+    print('Sütőpapírra rakjuk')
+    print('200 fokra és 15 percre')
+    print('Kivesszük a sütőből és lész. Jó étvagyat hozzá!')
+
+mirelit pizza('trapista','szalámi')
+mirelit pizza('mozarella','sonka')        
