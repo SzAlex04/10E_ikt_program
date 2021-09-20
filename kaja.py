3147ac2016de79b94e507a819f8b9f8e21aa95d0
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2021-09-20 17:19:28 +0200

    Update kaja.py

--- kaja.py
+++ kaja.py
@@ -5,8 +5,8 @@ def mirelit pizza (cheese_type; side, side_dish):
     print('Sütőpapírra rakjuk')
     print('200 fokra és 15 percre')
     print('Kivesszük a sütőből és lész. Jó étvagyat hozzá!')
-    print()
-    print()
+    print('Szórj rá reszelt ' + cheese_type + '-t.')
+    print('Szolgáld fel ' + side_dish + '-val/vel.')
 
 mirelit pizza('trapista','szalámi')
 mirelit pizza('mozarella','sonka')        
