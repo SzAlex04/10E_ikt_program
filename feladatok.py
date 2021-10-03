902935010190578a29fa9424163cbcfd09a505bd
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2021-10-03 18:19:54 +0200

    Add files via upload

--- feladatok.py
+++ feladatok.py
@@ -1,4 +1,4 @@
-moci = input (' Neked van motorod? ')
+moci = input (' Milyen típusú motorod van? ')
 ccm = int (input(' Hány köbcentis? Add meg egész számként: '))
 suly = float (input (' Hány kg-os? Add meg tizedes számként: '))
-print ('Neked van'+ moci +'motorod , ami'+ ccm + 'és' + suly +'tonnás' )
+print ('Neked van egy'+ moci +'motorod , ami'+ ccm + 'és' + suly +'kilógrammos' )
