8922fad49cdb5332d82f9d6775687f303312b609
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2021-10-03 15:42:08 +0200

    Add files via upload

--- feladatok.py
+++ feladatok.py
@@ -1,4 +1,4 @@
-tantargy = input (' Hanyas vagy cula? ')
-jegy = int(input(' 3 '))
-3.6 = float
-print ('Nem vészes, de nem is jó cula'+ tantargy +'kapott, jegy', 3.6)
+tip = input (' Milyen autó? ')
+ccm = int (input(' 1590 '))
+kilo = float (input (' 1.1 '))
+print ('Neked van'+ tip +'autód , ami'+ ccm + 'és' + kilo +'tonnás' )
