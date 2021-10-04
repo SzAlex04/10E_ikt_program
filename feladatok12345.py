2d79d4e5da3bcff51750c180246d121e7d0eaa7d
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2021-10-04 10:16:41 +0200

    Add files via upload

--- feladatok12345.py
+++ feladatok12345.py
@@ -34,3 +34,31 @@ b = int (input(' szám 2: '))
 c = int (input(' szám 3: '))
 
 print (a + b + c)
+
+
+print (' 6. feladat: ')
+
+f = int (input(' szám 1: '))
+
+g = int (input(' szám 2: '))
+
+print (f * g)
+
+
+print (' 7. feladat: ')
+
+m = int (input(' szám 1: '))
+
+n = int (input(' szám 2: '))
+
+print (m / n)
+
+
+print (' 8. feladat: ')
+
+x = int (input(' szám 1: '))
+
+y = int (input(' szám 2: '))
+
+z = float ( x / y)
+print ('A két szám hányadosa: ', z)
