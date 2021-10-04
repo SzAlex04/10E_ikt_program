473e8bbe00acef7ed5b2c2c88c07900978e3e88b
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2021-10-04 09:58:48 +0200

    Add files via upload

---
+++ feladatok12345.py
@@ -0,0 +1,36 @@
+print (' 1. feladat: ')
+
+name = input (' Hogy hívnak? ')
+print (' Szia '+ name + '!')
+
+
+print (' 2. feladat: ')
+
+print (' ********** ')
+
+print (' ooooooooo ')
+
+
+print (' 3. feladat: ')
+
+print (' *o*o*o*o*o*o*o*o*o ')
+
+
+print (' 4. feladat: ')
+
+number1 = 1
+
+number2 = 2
+
+print ((number1) + (number2))
+
+
+print (' 5. feladat: ')
+
+num1 = 5
+
+num2 = 6
+
+num3 = 7
+
+print ((num1) + (num2) + (num3))
