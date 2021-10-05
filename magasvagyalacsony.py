db23e529afa1a3f3e013f35782843773bafb37c6
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2021-10-05 18:39:39 +0200

    Add files via upload

---
+++ magasvagyalacsony.py
@@ -0,0 +1,8 @@
+x = int(input(' Add meg hány centiméter magas vagy: '))
+  
+y = int(input(' Add meg hány centiméter magas vagy: '))
+
+if ( x > y ):
+    print(' He,te zsiráf gyerek. ')
+else:
+    print(' Na, te meg törpe vagy. ')
