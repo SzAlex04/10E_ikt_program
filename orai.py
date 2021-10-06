813f2d38a1564dbf29cd2883ac2246b515ed4fa1
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2021-10-06 14:36:41 +0200

    Add files via upload

---
+++ orai.py
@@ -0,0 +1,9 @@
+vezeteknev = input (' Mi a vezetékneved? ') 
+ 
+keresztnev = input (' Mi a keresztneved? ')
+
+print (' A te neved ',vezeteknev,keresztnev)
+
+ev = int(input(' Hány éves vagy? '))
+
+print ('Te', 2021-ev ,' -ben születtél. ')
