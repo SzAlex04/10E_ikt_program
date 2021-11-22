41b98ac0eb496b292daca93bea9c66a49d58c5ec
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2021-11-22 10:33:28 +0100

    Add files via upload

--- feladatok.py
+++ feladatok.py
@@ -15,10 +15,50 @@ print('A szám',2*szam)
 
 print('4.feladat:')
 
-szam1 = 0
-szam2 = 1
-szam3 = 2
-szam4 = 3
-szam5 = 4
+for x in range(0,5):
+ print(x+x)
 
-print(szam1+szam2+szam3+szam4+szam5)
+print('5.feladat:')
+
+for x in range(0,10):
+ print(x*x)
+
+print('6.feladat:')
+
+for x in range(1,32):
+ print(x+x) 
+
+print('7.feladat:')
+
+for x in range(0,101,2):
+ print(x)
+
+print('8. feladat:')
+
+szam1 = int(input('Adjon meg egy számot!'))
+
+szam2 = int(input('Adjon meg egy másik számot!'))
+
+print(szam1*szam1,szam2*szam2)
+
+print('9. feladat:')
+for x in range (50,10,-2):
+    print(x)
+
+print('10. feladat')
+
+kerulet = int(input('Adjon meg egy számot!'))
+
+terulet = int(input('Adjon meg egy másik számot!'))
+
+print('A kerülete',4*kerulet,terulet*terulet)
+
+print('11. feladat')
+
+a = int(input('Adjon meg egy számot!'))
+
+b = int(input('Adjon meg egy másik számot!'))
+
+print('A kerülete',2*a+b)
+
+print('A területe',a*b)
